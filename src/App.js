7542c960b1dc19b65ce1a127338995ef6a3b755b
Victor Stone <victor.stone@gmail.com>
2021-11-25 13:35:38 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          "halo wurld" (font2, spin)
         </a>
       </header>
     </div>
